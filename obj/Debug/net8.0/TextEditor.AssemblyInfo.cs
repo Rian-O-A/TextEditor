1121caff41d9b6b92d3080e45ef80487133ffe5a
Rian Almeida <rianoliveira38@gmail.com>
2024-04-22 08:02:11 -0300

    update

--- obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
+++ obj/Debug/net8.0/TextEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57965eabe6a1e155b267050967148d8a48364b97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85c5f28ca7ce5e7b5730563a2fed6b693e9e205d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
